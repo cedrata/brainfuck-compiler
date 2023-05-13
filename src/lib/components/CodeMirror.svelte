ec03bb7d1f2ae53be59a437ede4d97713cc5c937
cedrata <81557163+cedrata@users.noreply.github.com>
2023-05-13 20:36:27 +0200

    [UPDATE] documenting types
    to help me understand code types are being added

--- src/lib/components/CodeMirror.svelte
+++ src/lib/components/CodeMirror.svelte
@@ -1,140 +1,160 @@
 <!-- The code below is released under public domain. -->
-
 <script context="module">
-	import { EditorView, minimalSetup, basicSetup } from 'codemirror'
-	import { ViewPlugin } from '@codemirror/view'
-	import { StateEffect } from '@codemirror/state'
-	export { minimalSetup, basicSetup }
-	</script>
-	
-	<script>
-	import { onMount, onDestroy, createEventDispatcher } from 'svelte'
-	const dispatch = createEventDispatcher()
-	
-	let dom
-	
-	let _mounted = false
+	import { EditorView, minimalSetup, basicSetup } from 'codemirror';
+	// import { ViewPlugin } from '@codemirror/view'
+	import { StateEffect } from '@codemirror/state';
+	export { minimalSetup, basicSetup };
+</script>
+
+<script>
+	import { onMount, onDestroy, createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	/**
+	 * @type {Element | DocumentFragment | undefined}
+	 */
+	let dom;
+
+	/**
+	 * @type {boolean}
+	 */
+	let _mounted = false;
 	onMount(() => {
-	  _mounted = true
-	  return () => { _mounted = false }
-	})
-	
-	export let view = null
-	
-	/* `doc` is deliberately made non-reactive for not storing a reduntant string
-	   besides the editor. Also, setting doc to undefined will not trigger an
-	   update, so that you can clear it after setting one. */
-	export let doc
-	
-	/* Set this if you would like to listen to all transactions via `update` event. */
-	export let verbose = false
-	
-	/* Cached doc string so that we don't extract strings in bulk over and over. */
-	let _docCached = null
-	
+		_mounted = true;
+		return () => {
+			_mounted = false;
+		};
+	});
+
+	/**
+	 * @type {EditorView | null}
+	 */
+	export let view = null;
+
+	/**
+	 * @type {string}
+	 * @description`doc` is deliberately made non-reactive for not storing a reduntant string
+	 * besides the editor. Also, setting doc to undefined will not trigger an
+	 * update, so that you can clear it after setting one.
+	 */
+	export let doc;
+
+	/**
+	 * @type {boolean}
+	 * @description Set this if you would like to listen to all transactions via `update` event.
+	 */
+	export let verbose = false;
+
+	/**
+	 * @type {string | null}
+	 * @description Cached doc string so that we don't extract strings in bulk over and over.
+	 */
+	let _docCached = null;
+
 	/* Overwrite the bulk of the text with the one specified. */
 	function _setText(text) {
-	  view.dispatch({
-		changes: {from: 0, to: view.state.doc.length, insert: text},
-	  })
+		view?.dispatch({
+			changes: { from: 0, to: view.state.doc.length, insert: text }
+		});
 	}
-	
-	const subscribers = new Set()
-	
+
+	const subscribers = new Set();
+
 	/* And here comes the reactivity, implemented as a r/w store. */
 	export const docStore = {
-	  ready: () => (view !== null),
-	  subscribe(cb) {
-		subscribers.add(cb)
-	
-		if (!this.ready()) {
-		  cb(null)
-		} else {
-		  if (_docCached == null) {
-			_docCached = view.state.doc.toString()
-		  }
-		  cb(_docCached)
+		ready: () => view !== null,
+		subscribe(cb) {
+			subscribers.add(cb);
+
+			if (!this.ready()) {
+				cb(null);
+			} else {
+				if (_docCached == null) {
+					_docCached = view?.state.doc.toString() ?? '';
+				}
+				cb(_docCached);
+			}
+
+			return () => void subscribers.delete(cb);
+		},
+		set(newValue) {
+			if (!_mounted) {
+				throw new Error('Cannot set docStore when the component is not mounted.');
+			}
+
+			const inited = _initEditorView(newValue);
+			if (!inited) _setText(newValue);
 		}
-	
-		return () => void subscribers.delete(cb)
-	  },
-	  set(newValue) {
-		if (!_mounted) {
-		  throw new Error('Cannot set docStore when the component is not mounted.')
-		}
-	
-				const inited = _initEditorView(newValue)
-		if (!inited) _setText(newValue)
-	  },
-	}
-	
-	export let extensions = minimalSetup
-	
+	};
+
+	export let extensions = minimalSetup;
+
 	function _reconfigureExtensions() {
-	  if (view === null) return
-	  view.dispatch({
-		effects: StateEffect.reconfigure.of(extensions),
-	  })
+		if (view === null) return;
+		view.dispatch({
+			effects: StateEffect.reconfigure.of(extensions)
+		});
 	}
-	
-	$: extensions, _reconfigureExtensions()
-	
+
+	$: extensions, _reconfigureExtensions();
+
 	function _editorTxHandler(tr) {
-	  this.update([tr])
-	
-	  if (verbose) {
-		dispatch('update', tr)
-	  }
-	
-	  if (tr.docChanged) {
-		_docCached = null
-		if (subscribers.size) {
-		  dispatchDocStore(_docCached = tr.newDoc.toString())
+		this.update([tr]);
+
+		if (verbose) {
+			dispatch('update', tr);
 		}
-		dispatch('change', {view: this, tr})
-	  }
+
+		if (tr.docChanged) {
+			_docCached = null;
+			if (subscribers.size) {
+				dispatchDocStore((_docCached = tr.newDoc.toString()));
+			}
+			dispatch('change', { view: this, tr });
+		}
 	}
-	
+
 	function dispatchDocStore(s) {
-	  for (const cb of subscribers) {
-		cb(s)
-	  }
+		for (const cb of subscribers) {
+			cb(s);
+		}
 	}
-	
+
 	// the view will be inited with the either doc (as long as that it is not `undefined`)
 	// or the value in docStore once set
+	/**
+	 * @param {string | undefined} initialDoc
+	 */
 	function _initEditorView(initialDoc) {
-	  if (view !== null) {
-		return false
-	  }
-	
-	  view = new EditorView({
-		doc: initialDoc,
-		extensions,
-		parent: dom,
-		dispatch: _editorTxHandler,
-	  })
-	  return true
+		if (view !== null) {
+			return false;
+		}
+
+		view = new EditorView({
+			doc: initialDoc,
+			extensions,
+			parent: dom,
+			dispatch: _editorTxHandler
+		});
+		return true;
 	}
-	
+
 	$: if (_mounted && doc !== undefined) {
-	  const inited = _initEditorView(doc)
-	  dispatchDocStore(doc)
+		const inited = _initEditorView(doc);
+		dispatchDocStore(doc);
 	}
-	
+
 	onDestroy(() => {
-	  if (view !== null) {
-		view.destroy()
-	  }
-	})
-	</script>
-	
-	<div class="codemirror" bind:this={dom}>
-	</div>
-	
-	<style>
+		if (view !== null) {
+			view.destroy();
+		}
+	});
+</script>
+
+<div class="codemirror" bind:this={dom} />
+
+<style>
 	.codemirror {
-	  display: contents;
+		display: contents;
 	}
-	</style>
-	
+</style>
