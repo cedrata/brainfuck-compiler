789d4e354f29e3279a75f02d0107708e80a26af6
cedrata <greggius9891@gmail.com>
2023-05-23 16:52:06 +0200

    Dynamic text editor height
    Get dimensions of text editor given window size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,39 @@
 	/** @type {DOMRect | null} */
 	let rect = null;
 	onMount(() => {
-		console.log("onMount")
-		rect = new DOMRect(0, 0, 100, 700)
+		console.log("Page mount")
+		// rect = new DOMRect(0, 0, 100, 700)
+		let toolBarElem = document.querySelector("#tool-bar")
+		let toolBarClientRect = null;
+
+		console.log("toolBarElem")
+		console.log(toolBarElem)
+
+		if (toolBarElem !== null) {
+			console.log()
+			toolBarClientRect = toolBarElem.getBoundingClientRect()
+		}
+
+		console.log("toolBarClientRect")
+		console.log(toolBarClientRect)
+
+		if (toolBarClientRect !== null) {
+			rect = new DOMRect(toolBarClientRect.x, toolBarClientRect.bottom, toolBarClientRect.width, window.innerHeight - toolBarClientRect.bottom)
+		}
+		console.log('rect')
+		console.log(rect)
+		// } else {
+		// 	temp = new DOMRect(100, 100, 500, 500)
+		// }
 	});
+
 </script>
 <div class="px-8">
+	<nav>
+		Navbar placeholder
+	</nav>
 	<section id="txt-edit" class="grid grid-cols-2">
-		<div class="col-start-1 col-span-2 row-start-1">Tool bar</div>
+		<div id="tool-bar" class="col-start-1 col-span-2 row-start-1">Tool bar</div>
 		<div class="col-start-1 row-start-2 row-span-2"><CodeMirror {rect}/></div>
 		<div class="col-start-2 row-start-2 row-span-2"><CodeMirror {rect}/></div>
 	</section>
