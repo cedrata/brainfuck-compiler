9d6ab332330511525decec0a2a0da62ac623715c
cedrata <81557163+cedrata@users.noreply.github.com>
2023-05-25 01:50:20 +0200

    [UPDATE] responsive layout
    two editors one under each other on smaller screens

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,40 +6,45 @@
 	/** @type {DOMRect | null} */
 	let rect = null;
 	onMount(() => {
-		console.log("Page mount")
+		console.log('Page mount');
 		// rect = new DOMRect(0, 0, 100, 700)
-		let toolBarElem = document.querySelector("#tool-bar")
+		let toolBarElem = document.querySelector('#tool-bar');
 		let toolBarClientRect = null;
 
-		console.log("toolBarElem")
-		console.log(toolBarElem)
+		console.log('toolBarElem');
+		console.log(toolBarElem);
 
 		if (toolBarElem !== null) {
-			console.log()
-			toolBarClientRect = toolBarElem.getBoundingClientRect()
+			console.log();
+			toolBarClientRect = toolBarElem.getBoundingClientRect();
 		}
 
-		console.log("toolBarClientRect")
-		console.log(toolBarClientRect)
+		console.log('toolBarClientRect');
+		console.log(toolBarClientRect);
 
 		if (toolBarClientRect !== null) {
-			rect = new DOMRect(toolBarClientRect.x, toolBarClientRect.bottom, toolBarClientRect.width, window.innerHeight - toolBarClientRect.bottom)
+			rect = new DOMRect(
+				toolBarClientRect.x,
+				toolBarClientRect.bottom,
+				toolBarClientRect.width,
+				window.innerHeight - toolBarClientRect.bottom
+			);
 		}
-		console.log('rect')
-		console.log(rect)
-		// } else {
-		// 	temp = new DOMRect(100, 100, 500, 500)
-		// }
+		console.log('rect');
+		console.log(rect);
 	});
-
 </script>
+
 <div class="px-8">
-	<nav>
-		Navbar placeholder
-	</nav>
-	<section id="txt-edit" class="grid grid-cols-2">
-		<div id="tool-bar" class="col-start-1 col-span-2 row-start-1">Tool bar</div>
-		<div class="col-start-1 row-start-2 row-span-2"><CodeMirror {rect}/></div>
-		<div class="col-start-2 row-start-2 row-span-2"><CodeMirror {rect}/></div>
+	<nav>Navbar placeholder</nav>
+	<section id="txt-edit" class="grid lg:grid-cols-2 max-md:grid-cols-1">
+		<div id="tool-bar" class="col-start-1 lg:col-span-2 row-start-1 h-8">Tool bar</div>
+		<div class="lg:col-start-1 max-md:row-start-2 lg:row-start-2 lg:row-span-2">
+			<CodeMirror {rect} />
+		</div>
+		<div class="lg:hidden h-8 " />
+		<div class="lg:col-start-2 max-md:row-start-3 lg:row-start-2 lg:row-span-2">
+			<CodeMirror {rect} />
+		</div>
 	</section>
 </div>
