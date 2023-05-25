c650a84e5d1753d5880906692962fdcfd544c9b3
cedrata <greggius9891@gmail.com>
2023-05-25 16:59:59 +0200

    [UPDATE] simple header placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,18 +5,24 @@
 	let editorHeight = 500;
 </script>
 
-<!-- <svelte:window on:resize={handleResize} /> -->
+<header class="h-16 bg-accent flex justify-center px-8">
+	<ul class="max-w-7xl w-full flex justify-between">
+		<li>IMAGE</li>
+		<li>CONTACT</li>
+	</ul>
+</header>
 
-<div class="px-8">
-	<nav>Navbar placeholder</nav>
-	<section id="txt-edit" class="grid lg:grid-cols-2 max-md:grid-cols-1">
-		<div id="tool-bar" class="col-start-1 lg:col-span-2 row-start-1 h-8">Tool bar</div>
-		<div class="lg:col-start-1 max-md:row-start-2 lg:row-start-2 lg:row-span-2">
-			<CodeMirror {editorHeight} />
+<section class="flex justify-center px-8">
+	<div class="max-w-7xl w-full flex justify-between">
+		<div id="txt-edit" class="grid lg:grid-cols-2 gap-x-16 max-md:grid-cols-1 w-full">
+			<div id="tool-bar" class="col-start-1 lg:col-span-2 row-start-1 h-8">Tool bar</div>
+			<div class="lg:col-start-1 max-md:row-start-2 lg:row-start-2 lg:row-span-2">
+				<CodeMirror {editorHeight} />
+			</div>
+			<div class="lg:hidden h-8 max-md:row-start-3" />
+			<div class="lg:col-start-2 max-md:row-start-4 lg:row-start-2 lg:row-span-2">
+				<CodeMirror {editorHeight} />
+			</div>
 		</div>
-		<div class="lg:hidden h-8 max-md:row-start-3" />
-		<div class="lg:col-start-2 max-md:row-start-4 lg:row-start-2 lg:row-span-2">
-			<CodeMirror {editorHeight} />
-		</div>
-	</section>
-</div>
+	</div>
+</section>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,6 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["garden"]
+    themes: ["light"]
   }
 }
