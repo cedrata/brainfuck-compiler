22248b260aa7c00404633a071f75ab65f970948d
cedrata <greggius9891@gmail.com>
2023-05-26 15:59:55 +0200

    [FIX] typo padding
    accidentally removed padding informations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 	let editorHeight = 500;
 </script>
 
-<header class="h-16 bg-accent flex justify-center">
+<header class="h-16 bg-accent flex justify-center px-8">
 	<div class="max-w-7xl w-full flex justify-between">
 		<a href="/" class="flex"><div class="self-center">BF-Compiler</div></a>
 		<a href="https://cedrata.github.io/portfolio" class="flex"
@@ -14,7 +14,7 @@
 	</div>
 </header>
 
-<section class="flex justify-center">
+<section class="flex justify-center px-8">
 	<div class="max-w-7xl w-full flex justify-between">
 		<div id="txt-edit" class="grid lg:grid-cols-2 gap-x-16 max-md:grid-cols-1 w-full">
 			<div id="tool-bar" class="col-start-1 lg:col-span-2 row-start-1 h-8">Tool bar</div>
