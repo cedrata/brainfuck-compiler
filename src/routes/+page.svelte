7d560d5927516fb6dcaed5f563c405d1cf2000b8
cedrata <greggius9891@gmail.com>
2023-05-26 15:58:14 +0200

    [UPDATE] vertical align items in navigation bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,16 @@
 	let editorHeight = 500;
 </script>
 
-<header class="h-16 bg-accent flex justify-center px-8">
-	<ul class="max-w-7xl w-full flex justify-between">
-		<li>IMAGE</li>
-		<li>CONTACT</li>
-	</ul>
+<header class="h-16 bg-accent flex justify-center">
+	<div class="max-w-7xl w-full flex justify-between">
+		<a href="/" class="flex"><div class="self-center">BF-Compiler</div></a>
+		<a href="https://cedrata.github.io/portfolio" class="flex"
+			><div class="self-center">Wunt sum Cedrata?</div></a
+		>
+	</div>
 </header>
 
-<section class="flex justify-center px-8">
+<section class="flex justify-center">
 	<div class="max-w-7xl w-full flex justify-between">
 		<div id="txt-edit" class="grid lg:grid-cols-2 gap-x-16 max-md:grid-cols-1 w-full">
 			<div id="tool-bar" class="col-start-1 lg:col-span-2 row-start-1 h-8">Tool bar</div>
