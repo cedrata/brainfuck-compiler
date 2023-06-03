89ed0026b3c99ed615cafce7f188ca78c16b4055
cedrata <81557163+cedrata@users.noreply.github.com>
2023-06-03 12:28:00 +0200

    [BUILD] static content for distributions

--- docs/app/immutable/chunks/1.63a74276.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.e9b04ee6.js";export{t as component};
--- docs/app/immutable/chunks/2.53d3d615.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.a9a4a60e.js";export{t as component};
